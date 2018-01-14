185cf737a1f80c147839486140d5248ea15dbc9d
Tatyana Scotce <tscotce@gmail.com>
2018-01-14 02:40:26 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
-  if board[index] == index.between? (0, 8)
+  if board[index] == "  "
     return true
   else 
     return false
